5dabb41aa97a07a84e3f3d6fcf08672560b40abf
PKParmar02 <163420770+PKParmar02@users.noreply.github.com>
2024-05-22 14:49:46 +0530

    Add files via upload

---
+++ firstprogram/firstprogram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello parth");
+            
+            // Variables
+            int x; //declaration 
+            x = 123; //initialization
+
+            int y;
+            y = 321;
+            Console.WriteLine(x+ y);
+            Console.WriteLine(y-x);
+            Console.WriteLine(y/x);
+            Console.WriteLine(x*y);
+
+            Console.ReadKey();
+        }
+    }
+}
